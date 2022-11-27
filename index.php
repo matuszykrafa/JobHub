d7bbd4da0be465264f69c52513b8ea4faf454047
Rafał Matuszyk <matuszykrafa@gmail.com>
2022-11-27 14:37:55 +0100

    routing init

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'Hi there 👋';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], "/");
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('', 'DefaultController');
+Routing::get('home', 'DefaultController');
+Routing::get('login', 'DefaultController');
+Routing::run($path);
