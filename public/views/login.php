cf4734d2d84a7bc5c5bdcea2dab6ec02c333b661
Rafał Matuszyk <matuszykrafa@gmail.com>
2022-11-27 16:36:29 +0100

    add login page

--- public/views/login.php
+++ public/views/login.php
@@ -7,19 +7,29 @@
 
 <body>
 <header>
-
+    <div class="header-buttons"></div>
+    <div class="header-title">
+        <a href="home">JobHub</a>
+    </div>
+    <div class="header-buttons">>
+        <a href="register">Rejestracja</a>
+    </div>
 </header>
 <div class="container">
-    <div class="logo">
-        <img src="public/img/logo.svg">
-    </div>
     <div class="login-container">
+        <div class="heading-container">
+            <div class="heading-content">
+                <img class="icon" src="public/img/login-icon.svg">
+                <span>Zaloguj</span>
+            </div>
+            <hr class="heading-line"/>
+        </div>
         <form class="login" action="login" method="POST">
             <div class="messages">
             </div>
-            <input name="email" type="text" placeholder="email@email.com">
-            <input name="password" type="password" placeholder="password">
-            <button type="submit">LOGIN</button>
+            <input class="input" name="email" type="text" placeholder="login">
+            <input class="input" name="password" type="password" placeholder="hasło">
+            <button class="button" type="submit">Login</button>
         </form>
     </div>
 </div>
